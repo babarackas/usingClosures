f89481c027ed1accdaf87ce1e7d8092283ba08f2
Philip Bazzard <pbazzard@gmail.com>
2017-03-10 01:53:03 +0000

    added first loaded die closure exercise

---
+++ loadedDie.js
@@ -0,0 +1,17 @@
+function makeLoadedDie() {
+  var list = [5, 4, 6, 1, 6, 4, 2, 3, 3, 5];
+  /* your code here */
+  var placement = 0
+  var result = list[0];
+  return function() {
+    /* your code here */
+    result = list[placement];
+    placement += 1;
+  }
+}
+
+var rollLoadedDie = makeLoadedDie();
+
+console.log(rollLoadedDie());  // 5
+console.log(rollLoadedDie());  // 4
+console.log(rollLoadedDie());  // 6
